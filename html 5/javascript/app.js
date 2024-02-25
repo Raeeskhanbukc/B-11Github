0c1b179c478c3475b39faff84dac0e10127f4267
Raeeskhanbukc <157282267+Raeeskhanbukc@users.noreply.github.com>
2024-02-25 08:32:00 +0500

    html 5 B11

---
+++ html 5/javascript/app.js
@@ -0,0 +1,27 @@
+// var pakola ="shabbir"
+// pakola ="raees"
+// alert(pakola)
+// pakola ="shabbir"
+// // alert(pakola)
+// // var weight = 125;
+// var salary = 5660 
+// alert(salary+5000)
+alert("welcome to javascript")
+// var student1 ='shabir'
+// var feesstudent1 = 1000
+// var finestudent1=500
+// var totalcollectionstudent1 =feesstudent1 + finestudent1
+
+// var employsalary1 = 25000
+// employsalary1 = 2500 + employsalaryl
+// alert("employsalary1")
+// var student1phoneNumber = 03458007635
+var aqibphNumber = "03456789000";
+var umerphNumber = "03456799999";
+// var aqib = 500;
+
+
+
+
+phNumber + umerphNumber
+alert(totalNumber)
